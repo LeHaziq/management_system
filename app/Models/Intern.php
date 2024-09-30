fa6530b0e3fdc28c13a5f5b394f4c91c798054b4
Haziq Aiman <haziqaimanfb@gmail.com>
2024-09-30 12:47:36 +0800

    update intern model fillables and cast

--- app/Models/Intern.php
+++ app/Models/Intern.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Intern extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'letter',
+        'education_level',
+        'education_year',
+        'school_university',
+        'skills',
+        'university',
+        'training_period',
+        'start_date',
+        'end_date',
+        'picture',
+        'resume',
+        'status',
+    ];
+
+    protected $casts = [
+        'skills' => 'array',
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
 }
