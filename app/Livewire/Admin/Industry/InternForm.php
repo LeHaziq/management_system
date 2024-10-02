e11b20d4aedf9beaa42e789da99a6fe3ffad760f
Haziq Aiman <haziqaimanfb@gmail.com>
2024-10-02 09:07:49 +0800

    fix intern form not saving properly

--- app/Livewire/Admin/Industry/InternForm.php
+++ app/Livewire/Admin/Industry/InternForm.php
@@ -4,13 +4,17 @@ namespace App\Livewire\Admin\Industry;
 
 use App\Livewire\BaseForm;
 use App\Models\Intern;
+use Carbon\Carbon;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Notifications\Notification;
 use Filament\Tables\Columns\TextColumn;
 use Illuminate\Support\Facades\Log;
@@ -20,7 +24,7 @@ class InternForm extends BaseForm
 {
     public ?Intern $intern = null;
 
-    public function mount(Intern $intern = null): void
+    public function mount()
     {
         $this->intern = $intern ?? new Intern();
         $this->data = $this->intern->toArray();
@@ -31,24 +35,61 @@ class InternForm extends BaseForm
     {
         return $form
             ->schema([
-                Section::make('Maklumat Asas')
+                Section::make('Maklumat Peribadi')
                     ->description('Maklumat asas mengenai individu')
                     ->schema([
                         TextInput::make('name')->required()->label('Nama'),
                         TextInput::make('ic')->required()->label('IC'),
                         TextInput::make('email')->required()->email()->label('E-Mel'),
                     ]),
+                Section::make('Maklumat Pengajian')
+                    ->description('Maklumat mengenai tahap pengajian')
+                    ->schema([
+                        TextInput::make('education_level')->label('Tahap Akademik'),
+                        TextInput::make('education_institution')->label('Sekolah/Universiti'),
+                        TextInput::make('year')->label('Tahun Akademik')->numeric(),
+                        TextInput::make('skill')->label('Kemahiran'),
+                    ]),
+                Section::make('Maklumat Latihan Industri')
+                    ->description('Maklumat mengenai latihan industri')
+                    ->schema([
+                        DatePicker::make('start_date')->label('Tarikh Mula Latihan'),
+                        DatePicker::make('end_date')->label('Tarikh Tamat Latihan'),
+                    ]),
+                Section::make('Dokumen')
+                    ->description('Muat naik dokumen yang diperlukan')
+                    ->schema([
+                        FileUpload::make('photo')->label('Foto'),
+                        FileUpload::make('resume')->label('Resume'),
+                    ]),
+                Section::make('Status')
+                    ->description('Status pelatih')
+                    ->schema([
+                        Select::make('status')->label('Status')->options([
+                            'diterima' => 'Diterima',
+                            'ditolak' => 'Ditolak',
+                            'aktif' => 'Aktif',
+                            'tamat' => 'Tamat',
+                        ])
+                            ->native(false),
+                    ]),
+
             ])
             ->statePath('data');
     }
 
     public function save()
     {
-        $this->form->getState();
+        $state = $this->form->getState();
 
-        $this->intern->fill([
-            ...$this->data,
-        ])->save();
+        // Recalculate internship period before saving
+        if ($state['start_date'] && $state['end_date']) {
+            $startDate = Carbon::parse($state['start_date']);
+            $endDate = Carbon::parse($state['end_date']);
+            $state['internship_period'] = $startDate->diffInMonths($endDate);
+        }
+
+        $this->intern->fill($state)->save();
 
         Notification::make()
             ->title('Berjaya')
