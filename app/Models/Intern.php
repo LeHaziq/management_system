832bf0f3e6bf3fb1681f5ce6d4a022ac3492a52f
Haziq Aiman <haziqaimanfb@gmail.com>
2024-10-02 09:08:24 +0800

    update intern model

--- app/Models/Intern.php
+++ app/Models/Intern.php
@@ -9,10 +9,24 @@ class Intern extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+
     protected $fillable = [
         'name',
         'ic',
         'email',
+        'education_level',
+        'education_institution',
+        'year',
+        'skill',
+        'internship_period',
+        'start_date',
+        'end_date',
+        'photo',
+        'resume',
+        'status',
     ];
-
 }
