d619c8b8075e65cf3dd52dd610c404fe70078114
Haziq Aiman <haziqaimanfb@gmail.com>
2024-10-02 16:21:50 +0800

    update dashboard widgets

--- app/Livewire/Admin/Project/ProjectOverview.php
+++ app/Livewire/Admin/Project/ProjectOverview.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Admin\Project;
 
+use App\Models\Project;
+use App\Models\Intern;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -9,10 +11,15 @@ class ProjectOverview extends BaseWidget
 {
     protected function getStats(): array
     {
+        $project = Project::where('status', 'Berjaya')->count();
+        $intern = Intern::where('status', 'Aktif')->count();
+
         return [
-            Stat::make('Unique views', '192.1k'),
-            Stat::make('Bounce rate', '21%'),
-            Stat::make('Average time on page', '3:12'),
+            Stat::make('Projek Aktif', $project)
+                ->icon('heroicon-m-arrow-trending-up')
+                ->color('danger'),
+            Stat::make('Pelatih Industri', $intern)
+                ->icon('heroicon-m-users'),
         ];
     }
 }
