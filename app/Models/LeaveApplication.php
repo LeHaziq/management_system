a8bb144e911bc4e2febc28a02ed43194188db2c9
Haziq Aiman <haziqaimanfb@gmail.com>
2024-10-03 09:23:38 +0800

    update leave model

--- app/Models/LeaveApplication.php
+++ app/Models/LeaveApplication.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LeaveApplication extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'intern_id',
+        'reason',
+        'start_date',
+        'end_date',
+        'leave_duration',
+        'status',
+    ];
+
+    public function intern(): BelongsTo
+    {
+        return $this->belongsTo(Intern::class);
+    }
 }
